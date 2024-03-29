cb6ef0fe8f15a4ab335588e9b4cadc27ada258b2
SarbasishChowdhury2002 <99132895+SarbasishChowdhury2002@users.noreply.github.com>
2024-03-29 21:52:51 +0530

    updated

---
+++ script.js
@@ -0,0 +1,18 @@
+var typed = new Typed(".text", {
+    strings: ["Programming" , "Web Development" , "Machine Learning"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+var toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.scrollY > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
