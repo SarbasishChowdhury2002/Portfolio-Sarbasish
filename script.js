9aaa65aea3b43cc79c1be1f37cc80e515f62c205
Sarbasish Chowdhury <99132895+SarbasishChowdhury2002@users.noreply.github.com>
2024-07-07 16:10:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Programming" , "Web Development" , "Machine Learning"],
+    strings: ["Programming" , "Web Development" , "Machine Learning", "Java Developer",],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
